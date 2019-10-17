564d7f47033cffafbc322ac06f1f2ad5ef2c9be1
Ben Baker <bsb1285@rit.edu>
2019-10-17 13:47:41 -0400

    Added a simple script for the camera to follow

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToFollow;
+
+    private Camera camera; //This Camera
+    [SerializeField] private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = objectToFollow.transform.position + offset;
+    }
+}
--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -10,7 +10,8 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rBody = GetComponent<Rigidbody>();    
+        rBody = GetComponent<Rigidbody>(); 
+        rBody.velocity = Vector3.zero;
     }
 
     public void Move(float x, float y) {
